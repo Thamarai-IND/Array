e75c696cf70b91289981e03727c8f20e31c06431
Thamarai-IND <thamarai.vivid@gmail.com>
2026-02-13 12:45:11 +0530

    reverse array naive approach implemented

--- reverse-array.js
+++ reverse-array.js
@@ -11,3 +11,22 @@ function reverseArray1(arr) {
 let arr1 = [7,4,2,5,6];
 const reponse1 = reverseArray1(arr1);
 console.log(reponse1);
+
+
+// naive approach
+
+function reverseArray2(arr) {
+    let low = 0;
+    let high = arr.length-1
+
+    while(low < high) {
+        [arr[low],arr[high]] = [arr[high],arr[low]];
+        low++;
+        high--
+    }
+    return arr;
+}
+
+let arr2 = [1,2,3,4,5];
+const reponse2 = reverseArray2(arr2);
+console.log(reponse2);
