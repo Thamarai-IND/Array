ba52148f5a45476473be46920c15cb29dbf1127f
Thamarai-IND <thamarai.vivid@gmail.com>
2026-02-16 01:28:33 +0530

    efficient solution for trapping the water units

--- trappingRainWater.js
+++ trappingRainWater.js
@@ -38,4 +38,57 @@ function maxWater(arr, n) {
 let arr = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
 let n = arr.length;
 
-console.log(maxWater(arr, n));
+console.log(maxWater(arr, n)); 
+// Time Complexity: O(N2). There are two nested loops traversing the array.
+// Space Complexity: O(1). No extra space is required.
+
+
+// efficient solution
+
+
+function findWater(arr, n) {
+  // left[i] contains height of tallest bar to the
+  // left of i'th bar including itself
+  let left = new Array(n);
+
+  // right[i] contains height of tallest bar to
+  // the right of ith bar including itself
+  let right = new Array(n);
+
+  // Initialize result
+  let water = 0;
+
+  // Fill left array
+  left[0] = arr[0];
+  for (let i = 1; i < n; i++) {
+    left[i] = Math.max(left[i - 1], arr[i]);
+  }
+
+  // Fill right array
+  right[n - 1] = arr[n - 1];
+  for (let i = n - 2; i >= 0; i--) {
+    right[i] = Math.max(right[i + 1], arr[i]);
+  }
+
+  // Calculate the accumulated water element by element
+  // consider the amount of water on i'th bar, the
+  // amount of water accumulated on this particular
+  // bar will be equal to min(left[i], right[i]) - arr[i]
+  // .
+  for (let i = 1; i < n - 1; i++) {
+    let x = Math.min(left[i - 1], right[i + 1]);
+    if (x > arr[i]) {
+      water += x - arr[i];
+    }
+  }
+
+  return water;
+}
+
+// Example usage
+let arr1 = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+let len = arr.length;
+console.log(findWater(arr1, len));
+
+// Time Complexity: O(N). Only one traversal of the array is needed, So time Complexity is O(N).
+// Space Complexity: O(N). Two extra arrays are needed, each of size N.
