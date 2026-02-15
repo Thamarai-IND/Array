65ded34d83fe3906ed0e8be9f31070dbf1773e03
Thamarai-IND <thamarai.vivid@gmail.com>
2026-02-16 00:33:02 +0530

    efficient approach for printLeaders

--- printLeaders.js
+++ printLeaders.js
@@ -42,3 +42,35 @@ function printLeadersNaive(arr) {
 let arr1 = [ 7, 10, 4, 3, 6, 5, 2 ];
 const result1 = printLeadersNaive(arr1);
 console.log(result1);
+
+// More efficient way - traverse the element from right side to keep track of the maximum
+
+/**
+ * Input: arr = [ 5, 8, 6, 10, 4,7 ]
+ * result array = [ 7, 10 ]
+ * then reverse the array
+ * Output: leader = [ 10, 7]
+ */
+
+function printLeadersEfficient(arr) {
+    let res = [];
+    let n = arr.length;
+    let currLead = arr[n-1];
+    res.push(currLead);
+
+    for(let i=n-2; i>=0; i--) {
+        if(arr[i] >= currLead) {
+            currLead = arr[i];
+            res.push(currLead);
+        }
+    }
+
+    for(let i=res.length-1; i>=0; i--) {
+        console.log(res[i]);
+    }
+    return res.reverse();
+}
+
+let arr2 = [ 5, 8, 6, 10, 4,7 ];
+const result2 = printLeadersEfficient(arr2);
+console.log("efficient way : ",result2);
