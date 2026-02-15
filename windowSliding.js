652ae0f44f015ee35395714879f82b070d764f8c
Thamarai-IND <thamarai.vivid@gmail.com>
2026-02-16 02:57:46 +0530

    Efficient solution for window sliding technique to find the maximum sum of slide window

--- windowSliding.js
+++ windowSliding.js
@@ -28,3 +28,32 @@ let array1 = [ 10, 5, -2, 20, 1 ];
 let k1 = 3;
 const result1 = maxSumSlidingWindow(array1,k1);
 console.log(result1);
+
+// Time complexity : O(N*K) ans Space complexity : O(1)
+
+// Efficient Solution
+
+function maxSumSlidingWindowEfficient(arr,k) {
+    let curr = 0;
+
+    for(let i=0; i<k; i++) {
+        curr += arr[i];
+    }
+
+    let res = curr;
+    let n = arr.length;
+
+    for(let i=k; i<n; i++) {
+        curr += (arr[i] - arr[i-k]);
+        res = Math.max(res,curr);
+    }
+
+    return res;
+}
+
+let array2 = [ 1, 8, 30, -5, 20, 7 ];
+let k2 = 4;
+const result2 = maxSumSlidingWindowEfficient(array2,k2)
+console.log(result2); // 53
+
+// Time Complexity : O(N) and Space Complexity : O(1)
