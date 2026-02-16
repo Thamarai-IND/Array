60b33c7e933e9f792e8f62881cdd8b9585cc6836
Thamarai-IND <thamarai.vivid@gmail.com>
2026-02-17 00:08:06 +0530

    identify the equilibrium point in an array using efficient approach

--- equilibrium.js
+++ equilibrium.js
@@ -30,3 +30,30 @@ const response1 = ePoint1(array1);
 response1 ? console.log("we found equilibrium point") : console.log("we not found equilibrium point");
 
 
+// Efficient Solution
+
+function ePoint2(arr) {
+    let rs = 0;
+
+    for(let i=0; i<arr.length; i++) {
+        rs += arr[i];
+    }
+
+    let ls = 0;
+    for(let i=0; i<arr.length; i++) {
+        rs -= arr[i];
+
+        if(ls == rs) {
+            return true;
+        }
+        ls += arr[i];
+    }
+    return false;
+}
+
+let array2 = [3,4,8,-9,9,7];
+const response2 = ePoint2(array2);
+response2 ? console.log("we found equilibrium point") : console.log("we not found equilibrium point");
+
+
+// time complexity O(N) and auxiliary space complexity O(1)
